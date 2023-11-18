404a89632c1e3596cfc2915e57bb3f5ead0a02cd
Casper Wilstrup <casper.wilstrup@abzu.ai>
2023-11-18 12:37:36 +0100

    Improvement to line charts

--- singlecase/plotting/__init__.py
+++ singlecase/plotting/__init__.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from singlecase.data import Data
 
-def line_chart(data: Data, dvars: List[str] = None, num_per_row: int = 1, phases: List[str] = None, figure_size: Tuple[int, int] = (10, 8), y_range: Tuple[float, float] = None, title: str = None):
+def line_chart(data: Data, dvars: List[str] = None, num_per_row: int = 1, phases: List[str] = None, figure_size: Tuple[int, int] = (10, 8), y_range: Tuple[float, float] = None, title: str = None, filename=None, dpi=300):
     """
     Plot the selected dependent variables on a line chart, with separate colors for each phase.
 
@@ -25,7 +25,7 @@ def line_chart(data: Data, dvars: List[str] = None, num_per_row: int = 1, phases
     phase_colors = plt.get_cmap('tab10', len(phases))
 
     rows = (len(dvars) - 1) // num_per_row + 1
-    fig = plt.figure(figsize=figure_size)
+    fig = plt.figure(figsize=figure_size, dpi=dpi)
 
     if title is not None:
         fig.suptitle(title)
@@ -41,13 +41,16 @@ def line_chart(data: Data, dvars: List[str] = None, num_per_row: int = 1, phases
     min_y -= y_diff * 0.05
     max_y += y_diff * 0.05
 
+    xnames = list(data._df.index)
+
     for idx, dvar in enumerate(dvars):
         ax = fig.add_subplot(rows, num_per_row, idx + 1)
 
         for i, phase in enumerate(phases):
             phase_data = data.phase_data(phase, dvar)
-            ax.plot(phase_data, label=f"Phase: {phase}", color=phase_colors(i), zorder=3)
             ax.scatter(phase_data.index, phase_data, color=phase_colors(i), zorder=3) 
+            phase_data = phase_data.interpolate()
+            ax.plot(phase_data, label=f"Phase: {phase}", color=phase_colors(i), zorder=3)
 
         ax.set_ylim(min_y, max_y)
 
@@ -55,11 +58,18 @@ def line_chart(data: Data, dvars: List[str] = None, num_per_row: int = 1, phases
             ax.set_ylabel(data.dvar_units[dvar])
 
         ax.set_xlabel(data.index)
-        ax.set_title(f'{dvar} over sessions')
+
+        ax.set_xticks(range(len(xnames)))
+        ax.set_xticklabels(xnames)
+
+        ax.set_title(f'{dvar}')
         
         # Only add legend if there is more than one phase
         if len(phases) > 1:
             ax.legend()
 
     plt.tight_layout()
-    plt.show()
+    if filename is not None:
+        plt.savefig(filename)
+    else:
+        plt.show()
